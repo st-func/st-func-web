1491d65adb0ecb731ba4cfc57742896304f3308f
st_func <st_func@outlook.jp>
2024-04-02 22:51:50 +0900

    Sampleアプリケーションの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+// Components
+import Home from './Home';
+import Calculator from './Calculator';
+
+const App: React.FC = () => {
+  const [calcMode, setCalcMode] = useState<'sum' | 'product'>('sum');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calc">Calculator</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calc" element={<Calculator calcMode={calcMode} setCalcMode={setCalcMode} />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
